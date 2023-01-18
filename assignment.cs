b067fdf737ae2d43b057954e6f90b1326179ee29
starryheadedghost <122577617+starryheadedghost@users.noreply.github.com>
2023-01-17 17:51:43 -0700

    finished the assignment

---
+++ assignment.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    string cat = "Shia";
+    string dog = "Moon-Moon";
+    char names = 'S';
+    char name = 'M';
+    double petrooms = 10.5D;
+    double bathrooms = 3.9D;
+    float walls = 100.55f;
+    float floortiles = 522.64f;
+    bool catsfed = true;
+    bool dogsfed = true;
+    //I ran out of animal related things 
+    int yeah = 5;
+    int no = 7;
+    
+     catsfed = false;
+     no = 123456;
+      
+      /*my consoles also were
+      weirdly over-indented*/
+      
+     Console.WriteLine(cat);
+     Console.WriteLine(dog);
+     Console.WriteLine(names);
+     Console.WriteLine(name);
+     Console.WriteLine(petrooms);
+     Console.WriteLine(bathrooms);
+     Console.WriteLine(walls);
+     Console.WriteLine(floortiles);
+     Console.WriteLine(catsfed);
+     Console.WriteLine(dogsfed);
+     Console.WriteLine(yeah);
+     Console.WriteLine(no);
+    }
+  }
+}
